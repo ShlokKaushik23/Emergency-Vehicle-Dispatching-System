a18c5f13ff7e67e87dd03147c2eb9d2d3e3da8ee
Shlok Kaushik <shlokkaushik23@gmail.com>
2020-12-08 03:15:58 +0530

    ADA PROJECT

---
+++ src/DAA/main.java
@@ -0,0 +1,27 @@
+package DAA;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+
+public class main extends Dijkstra {
+    public static Vertex zip = null;
+    public main(Vertex argTarget, double argWeight) {
+
+            super(argTarget, argWeight);
+
+        }
+        public static void main(String[] args) throws IOException{
+
+        new Vehicle();
+        new Distance();
+
+        String zipcode = JOptionPane.showInputDialog("Enter the zipcode");
+        System.out.print("Given zipcode is :"+zipcode+"\n");
+
+        Vertex Zip = new Vertex(zipcode);
+        System.out.println("\n"+"Using Dijstra's Algorithm"+"\n"+"shortest paths from given zipcode to other zipcodes are displayed below");
+        Zip.vertexrun(Zip);
+
+    }
+}
